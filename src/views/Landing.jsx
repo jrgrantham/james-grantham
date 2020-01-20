02b1eb74b62611d61db1ffef23513b88dc2be3af
James Grantham <james.r.grantham@gmail.com>
2020-01-20 22:12:53 +0000

    add window height calculator and styling

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import jamesPhoto from "../../src/images/james.jpg";
 
-import { transition, greyMedium, borderRad, blackOff } from "./styling";
+import { transition, greyMedium, borderRad, blackOff, greyDark } from "./styling";
 
 export default function Landing() {
   // document.body.style.background = appColor;
@@ -11,66 +11,89 @@ export default function Landing() {
 
   return (
     <Position>
-      <LandingStyle>
+      <Header>
+        <Link to="/phonenumbers">
+          <h5 style={contactMe} >contact me</h5>
+        </Link>
+      </Header>
+      <Content id='content' >
         <h2>James Grantham</h2>
-        <div>
+        <div className='img '>
           <img src={jamesPhoto} alt="" />
         </div>
-        <Career>
-          <h4>Full-stack Web Developer</h4>
-          <h4>Mechanical Engineer</h4>
-        </Career>
-        <Buttons>
-          <Link to="/phonenumbers">
-            <h5>Social</h5>
-          </Link>
-          <Link to="/professional">
-            <h5>Professional</h5>
-          </Link>
-        </Buttons>
-      </LandingStyle>
+        <div>
+        	<h4>Mechanical Engineer</h4>
+          <p>turned</p>
+        	<h4>Full-stack Web Developer</h4>
+        </div>
+        <Link to="/professional">
+          <h5>Further information</h5>
+        </Link>
+      </Content>
+      <Footer></Footer>
     </Position>
   );
 }
 
+// window.onresize = function(){window.location.reload()}
+
+let vh = (window.innerHeight < 630) ? 630 : window.innerHeight
+console.log(vh)
+
 const Position = styled.div`
-  height: 100%
+  width: 100%
+  height: ${vh}px
   display: flex
   flex-direction: column
-  justify-content: center
-
-  @media (min-height: 850px) {
-    min-height: 100vh
-  }
+  justify-content: space-between
+  // border: 10px solid ${greyDark}
 `;
 
-const LandingStyle = styled.div`
-  margin: 40px
-  height: 520px
+const Header = styled.div`
+  background-color: brown
+  width: 100%
+`;
+
+const contactMe = {
+  textAlign: 'right',
+  color: 'white',
+  padding: '20px'
+}
+
+const Content = styled.div`
+  // margin: 40px
+  // padding: 30px 0
+  height: 70vh
+  min-height: 30rem
+  max-height: 50rem
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
   // border: 2px solid green
 
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .img {
+    width: 90%
+    // max-width: 700px
+    min-height: 20px
+    max-height: 30vh
+    object-fit: contain
+    
+    // border: 5px solid red
+  }
+  
   img {
-    width: 300px
+    max-width: 100%
+    max-height: 100%
     border-radius: ${borderRad}
   }
-`;
-
-const Career = styled.div`
-  display: flex
-  flex-direction: column
-  justify-content: space-between
-  height: 80px
-`;
-
-const Buttons = styled.div`
-  display: flex
-  flex-direction: column
-  justify-content: space-between
-  height: 130px
 
   h5 {
     padding: 1rem 0
@@ -85,4 +108,20 @@ const Buttons = styled.div`
       }
     }
   }
+
+  h4 {
+    // height: 80px
+    // margin: 10px 0
+  }
+`;
+
+const Footer = styled.div`
+  background-color: brown;
+  height: 10px
+  // clip-path: polygon(
+  //   0 0, /* left top */
+  //   100% 80%, /* right top */
+  //   100% 100%, /* right bottom */
+  //   0 100% /* left bottom */
+  // );
 `;
