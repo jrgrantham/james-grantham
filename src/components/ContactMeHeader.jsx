8517971ec6aab46f91b6497ceb50a83c71d1c6f8
James Grantham <james.r.grantham@gmail.com>
2020-01-21 23:23:29 +0000

    Add phone and email to landing page header

--- src/components/ContactMeHeader.jsx
+++ src/components/ContactMeHeader.jsx
@@ -7,41 +7,50 @@ export default function ContactMeHeader(props) {
   const [hidden, setHidden] = useState(true);
   console.log(hidden);
 
+  const Header = styled.div`
+    display: flex
+    justify-content: ${hidden ? "flex-end" : "space-evenly"} 
+    align-items: center
+    background-color: brown
+    min-height: ${headerHeight}px
+    width: 100vw
+    max-width:  1000px
+    position: fixed
+    top: 0
+  `;
+
+  const button = {
+    cursor: "pointer",
+    margin: "0 10px",
+    minWidth: "80px",
+    border: `1px solid ${whiteOff}`,
+    borderRadius: borderRad,
+    color: whiteOff,
+    padding: "0 10px"
+  };
+
   return (
     <Header>
-      <Link to='/phonenumbers'>
+      {hidden ? null : (
+        <>
+          <Link to="/phonenumbers">
+            <h5 style={button}>Phone</h5>
+          </Link>
+          <div>
+            <h5 style={button}>Email</h5>
+          </div>
+        </>
+      )}
+      <div>
         <h5
-          style={contactMe}
+          style={button}
           onClick={() => {
             setHidden(!hidden);
-            document.body.scrollTop = 0; // For Safari
-            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
           }}
         >
-          contact me
+          {hidden ? "contact me" : "hide "}
         </h5>
-      </Link>
+      </div>
     </Header>
   );
 }
-
-const Header = styled.div`
-  display: flex
-  justify-content: flex-end
-  align-items: center
-  background-color: brown
-  min-height: ${headerHeight}px
-  width: 100vw
-  max-width:  1000px
-  position: fixed
-  top: 0
-`;
-
-const contactMe = {
-  cursor: "pointer",
-  margin: "0 10px",
-  border: `1px solid ${whiteOff}`,
-  borderRadius: borderRad,
-  color: whiteOff,
-  padding: "0 10px"
-};
