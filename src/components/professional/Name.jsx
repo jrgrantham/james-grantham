34c43e8d9cd9048a1cfc0cd6e2e1bf1620e08d5f
James Grantham <james.r.grantham@gmail.com>
2020-04-06 18:07:35 +0100

    animate back button

--- src/components/professional/Name.jsx
+++ src/components/professional/Name.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import useDarkMode from "../../hooks/useDarkMode";
 import { Link } from "react-router-dom";
@@ -12,14 +12,61 @@ export default function Name() {
 
   return (
     <StlyedLink className="toggle darkmode name" to="/">
-      <div className="container arrow">
+      <StyledAnimation className="container">
         <img src={arrow} alt="back" />
-      </div>
+      </StyledAnimation>
+      {/* <div className="container arrow">
+        <img src={arrow} alt="back" />
+      </div> */}
       <h2 className="toggle darkmode">James Grantham</h2>
       <div className="container"></div>
     </StlyedLink>
   );
 }
+
+// Create the keyframes
+const move = keyframes`
+  0%   { transform: scale(1,1)      translateX(0); }
+  10%  { transform: scale(0.9,1.1)   translateX(0); }
+  30%  { transform: scale(1.1,0.9)   translateX(50px); }
+  50%  { transform: scale(0.95,1.05) translateX(0); }
+  57%  { transform: scale(1,1)      translateX(5px); }
+  64%  { transform: scale(1,1)      translateX(0); }
+  100% { transform: scale(1,1)      translateX(0); }
+`;
+
+// create a component that will move everything we pass in
+const StyledAnimation = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${professionalColor};
+  border-radius: 35%;
+  padding: 10px;
+  transition: all ${transition};
+
+  animation-duration: ${2}s;
+  // animation-iteration-count: infinite;
+  // animation-timing-function: bounce;
+  // animation-timing-function: cubic-bezier(0.280, 0.840, 0.420, 1)
+
+  // animation-name: ${move};
+
+  @media (max-width: ${mediaBreak}) {
+    display: none;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (pointer: fine) {
+    &:hover {
+      animation-name: ${move};
+    }
+  }
+`;
 
 const StlyedLink = styled(Link)`
   display: flex;
@@ -40,7 +87,7 @@ const StlyedLink = styled(Link)`
 
     @media (pointer: fine) {
       &:hover {
-        color: ${professionalColor};
+      color: ${professionalColor};
       }
     }
   }
@@ -54,26 +101,26 @@ const StlyedLink = styled(Link)`
     }
   }
 
-  .arrow {
-    display: flex;
-    background: ${professionalColor};
-    border-radius: 35%;
-    padding: 10px;
-    transition: all ${transition};
+  // .arrow {
+  //   display: flex;
+  //   background: ${professionalColor};
+  //   border-radius: 35%;
+  //   padding: 10px;
+  //   transition: all ${transition};
 
-    @media (pointer: fine) {
-      &:hover {
-        transform: scale(1.3);
-      }
-    }
+  //   @media (pointer: fine) {
+  //     &:hover {
+  //       transform: scale(1.3);
+  //     }
+  //   }
 
-    @media (max-width: ${mediaBreak}) {
-      display: none;
-    }
+  //   @media (max-width: ${mediaBreak}) {
+  //     display: none;
+  //   }
 
-    img {
-      width: 100%;
-      height: auto;
-    }
-  }
-`
+  //   img {
+  //     width: 100%;
+  //     height: auto;
+  //   }
+  // }
+`;
